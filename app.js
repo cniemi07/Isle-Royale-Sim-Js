f9679c6cdab544cdaf6db216439c816ee494c217
cniemi07 <cpniemi@mtu.edu>
2026-06-06 00:14:34 -0400

    More stylization and built a chart using chart.js

--- app.js
+++ app.js
@@ -1,73 +1,46 @@
 
+const canvas = document.getElementById("pop-chart").getContext("2d");
 
-//adding elements
-//ul.append(li)
-
-//modifying text
-/*
-const fL = document.querySelector("ul")
-const bodyE1 = document.querySelector("body")
-const newItem = document.createElement("li");
-const heading = document.getElementById("main-heading")
-const btn = document.getElementById("btn");
-const para = document.getElementById("para")
-const menu = document.getElementById("menu");
-const selectMenu = document.getElementById("select-menu")
-const toggleBtn = document.getElementById("toggle-btn")
-selectMenu.addEventListener("change", (event) =>{
-    alert(`You selected: ${event.target.value}`)
-    console.log(`You selected: ${event.target.value}`)
-})
-toggleBtn.addEventListener("click", ()=> menu.classList.toggle("show"))
-let isBgColorGrey = true;
-.addEventListener("")
-
-function toggleColor(){
-    bodyE1.style.backgroundColor = isBgColorGrey ? "lightgrey" : "grey";
-    isBgColorGrey = !isBgColorGrey;
+const data = {
+       years: [1980,1981,1982,1983,1984,1985,1986,1987,1988,1989,
+                1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,
+                2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,
+                2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,
+                2020,2022,2023,2024,2026],
+        wolves: [50,30,14,23,24,22,20,16,12,11,
+                 15,12,12,13,15,16,22,24,14,25,
+                 29,19,17,19,29,30,30,21,23,24,
+                 19,16,9,8,9,3,2,2,2,14,
+                 14,28,31,30,37],
+        moose:  [664,650,700,900,811,1062,1025,1380,1653,1397,
+                 1216,1313,1600,1880,1800,2400,1200,500,700,750,
+                 850,900,1000,900,750,540,385,450,650,530,
+                 510,515,750,975,1050,1250,1300,1600,1500,2060,
+                 null,1346,967,840,524]
 }
-btn.addEventListener("click", toggleColor)
-const intervalID = setInterval(toggleColor, 1000)
-setTimeout(()=>{clearInterval(intervalID)}, 5000)
-heading.setAttribute("class","main-heading")
-console.log(`${heading.outerHTML}`)
-
-newItem.textContent = heading.outerHTML ;
-fL.appendChild(newItem)
-
-console.log(fL.innerText);
-console.log(fL.textContent);
-console.log(fL.innerHTML);
-//fL.removeChild(newItem)
-
-
-fetch("https://pokeapi.co/api/v2/pokemon/eevee")
-    .then(response => {
-        if(!response.ok){
-            throw new Error("Could not fetch");
+new Chart(canvas, {type: 'line', data: {
+    labels: data.years,
+    datasets: [
+        {
+            label: "wolves",
+            data:data.wolves,
+            borderColor: 'red'
+        },
+        {
+            label: "moose",
+            data: data.moose,
+            borderColor: "blue"
         }
-        return response.json();
-
-    })
-    .then(data => console.log(data))
-    .catch(error  => console.error(error));
-*/
-
-async function fetchData(){
-    try{
-        const pokemonName = document.getElementById("pokemon-name").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-
-        if (!response.ok){
-            throw new Error("could not fetch");
-        }
-        const data = await response.json();
-        const sprite = data.sprites.front_default;
-        const imgElement = document.getElementById("sprite")
-        imgElement.src= sprite;
-        imgElement.style.display = "block";
+    ]
+}, options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Population Chart'
+      }
     }
-    catch(error){
-        console.error(error)
-    }
-}
+  },})
